b8718e147879a364fbdf174e3ce2c84e999a732b
Markus Witt <feuerrot@feuerrot.org>
2016-02-08 05:05:52 +0100

    mehr Stabilität

--- vnc_server.py
+++ vnc_server.py
@@ -12,7 +12,7 @@ p_protoversion = b'RFB 003.003\n'
 p_securityhandshake = b'\x00\x00\x00\x01'
 p_serverinit = b'\xFF\xFF\xFF\xFF\x20\x20\x01\x01\xFF\xFF\xFF\xFF\xFF\xFF\x00\x00\x00\x00\x00\x00'
 p_bell = b'\x02'
-p_much = b'\xFF\xFF\xFF\xFF'
+p_much = b'\x00\x00\xFF\xFF'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
@@ -22,26 +22,31 @@ while True:
 	conn, addr = s.accept()
 	print("Accepted connection from {}".format(addr))
 	log.write("Accepted connection from {}\n".format(addr))
+	log.flush()
 
-	conn.send(p_protoversion)
-	data = conn.recv(1024)
-	print(data)
-	
-	conn.send(p_securityhandshake)
-	data = conn.recv(1024)
-	print(data)
-
-	conn.send(p_serverinit)
-	conn.send(p_much)
-	conn.send(b'!'*0xFFFFFFFF)
-	data = conn.recv(1024)
-	print(data)
-
-	while True:
+	try:
+		conn.send(p_protoversion)
 		data = conn.recv(1024)
-		if not data:
-			break
 		print(data)
+		
+		conn.send(p_securityhandshake)
+		data = conn.recv(1024)
+		print(data)
+
+		conn.send(p_serverinit)
+		conn.send(p_much)
+		conn.send(b'!'*0xFFFF)
+		data = conn.recv(1024)
+		print(data)
+
+		while True:
+			data = conn.recv(1024)
+			if not data:
+				break
+			print(data)
+	except:
+		conn.close()
+		pass
 	conn.close()
 s.close()
 log.close()
